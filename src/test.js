fab344cfe9d767caf3dfaaad2bfb651b8a000df8
denis zatsarin <dzatsarin@korusconsulting.ru>
2018-02-22 16:00:48 +0300

    init

---
+++ src/test.js
@@ -0,0 +1,13 @@
+import _ from 'lodash'
+import {log} from './inc.js'
+
+const arr = ["hello","webpack", "!"]
+
+
+log("hello webpack!")
+
+log(_.join(["hello","webpack", "!"],' '))
+
+log(arr.reduce((p, c) => p += c, ""))
+
+//[...arr].map((x) => a(x))
